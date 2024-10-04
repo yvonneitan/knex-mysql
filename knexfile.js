f0d5157a97071448674a0b7047303f176758e944
Yvonne Itan <itan3040@mylaurier.ca>
2024-10-04 10:25:40 -0700

    Add datatables

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+export default{
+  client:"mysql2",
+  connection:{
+    host:"127.0.0.1",
+    user:"root",
+    password:"rootroot",
+    database:"knex_intro",
+    charset:"utf8"
+  }
+
+}
---
+++ migrations/20241004164453_users_posts.js
@@ -0,0 +1,28 @@
+
+export function up(knex) {
+    //create tables
+    return knex.schema.createTable("users",function(table){
+    table.increments("id");
+    table.string("name").notNullable();
+  
+     }).createTable("posts",function(table){
+        table.increments("id");
+        table.integer("user_id").unsigned().notNullable();
+        table.string("title",30).notNullable();
+        table.string("content").notNullable();
+        table
+        .foreign("user_id")
+        .reference("id")
+        .inTable("user")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+});
+  
+};
+
+
+export function down(knex) {
+    //drop tables
+    return knex.schema.dropTable("users");
+  
+};
